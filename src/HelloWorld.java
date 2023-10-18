535c0128496513bf226e0add0c6a911f143e4fae
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-10-17 22:30:27 -0300

    tipo de dados

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+    /*
+    *Tipos de dados
+    * Números (int, double, float, long )
+    * Texto (strig)
+    * Booleanos (boolean)
+    *
+    */
+        int dadoDoTipoInt = 10;
+        double dadoDoTipoDouble = 3.14;
+        float dadoDoTipoFloat = 3.14f; /* Em float é preciso colocar o f na frente, senão o java não identifica e fica como erro de identificação */
+        long dadoDoTipoLong = 8984372984237479823L; /*O long comporta um número maior que o número do int*/
+        String dadoDoTipoString = "Colocar o textto da forma que desejo";
+        boolean dadoDoTipoBoolean = false;
+
+    }
+}
