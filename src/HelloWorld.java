05e23021a628d4548637b9b5a3afef807d59b489
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-11-06 22:25:55 -0300

    iniciando estrutura de repetição

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,12 +7,23 @@ public class HelloWorld {
     * Booleanos (boolean)
     *
     */
-        int dadoDoTipoInt = 10;
+        int dadoDoTipoInt = 11;
         double dadoDoTipoDouble = 3.14;
         float dadoDoTipoFloat = 3.14f; /* Em float é preciso colocar o f na frente, senão o java não identifica e fica como erro de identificação */
         long dadoDoTipoLong = 8984372984237479823L; /*O long comporta um número maior que o número do int*/
-        String dadoDoTipoString = "Colocar o textto da forma que desejo";
+        String dadoDoTipoString = "Colocar o texto da forma que desejo";
         boolean dadoDoTipoBoolean = false;
 
+        //if - else
+        if(dadoDoTipoInt == 10) {
+            System.out.println("Entrou no if");
+        }else if (dadoDoTipoInt == 12){
+            System.out.println("Entrou no else");
+        }
+        else{
+            System.out.println("Entrou no segundo else");
+        }
+
+        // While => enquanto algum valor for verdadeiro faça algo
     }
 }
