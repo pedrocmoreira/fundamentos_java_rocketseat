08c906d097bbd7c6bcdb89bcb332223daa255891
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-11-20 11:01:11 -0300

    hashMap

---
+++ src/Colecoes.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Colecoes {
+    public static void main(String[] args) {
+        Map<String, Integer> notas = new HashMap<>();
+        notas.put("Pedro", 9);
+        notas.put("Vini", 10);
+        notas.put("Elias", 10);
+
+//        var nota = notas.get("Elias");
+//        System.out.println(nota);
+
+        for(Map.Entry<String, Integer>entry :notas.entrySet()){
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+
+            System.out.println("A chave é " + key + " e o valor é " + value);
+        }
+    }
+}
