04babdd89f8520924dabaff5e0799af01dc649c7
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-11-20 10:50:09 -0300

    estrutura de dados baseado em lista

---
+++ src/EstruturaDeDadosJava.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EstruturaDeDadosJava {
+    public static void main(String[] args) {
+        //Lista
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Pedro Moreira");
+        nomes.add("Vini");
+        nomes.add("Elias");
+
+//        System.out.println(nomes.get(2));
+//        for(String nome: nomes){
+//            System.out.println("O nome é " + nome);
+//        }
+
+        nomes.forEach(nome -> System.out.println("O nome é " + nome));
+    }
+}
